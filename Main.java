a022e22b80c2815d96f0b7b0c332cc69514ab826
Enes <enesfplt@gmail.com>
2024-07-02 12:38:04 -0400

    Tic-Tac-Toe

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        TicTacToe ticTacToe = new TicTacToe();      
+        TicTacToe ticTacToe = new TicTacToe();           
     }
- 
 }
